744ae6258d231c560644b7d62cb9fba23984fcec
Patrick Xin <patrick.xin.dev@gmail.com>
2022-01-01 05:52:58 -0800

    initial commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+      <h1 className="text-4xl font-bold text-pink-600">
+        NextJs, Tailwind, Typescript Starter
+      </h1>
+    </div>
+  );
+}
